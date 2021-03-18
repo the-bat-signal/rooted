bc74563de401fd29d9d1b76795821cb3a62e3d76
Elijah Sciammas <eli.sciammas@gmail.com>
2021-03-18 19:28:42 -0400

    Feat: deck.gl data vis overlay on sample map

--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,7 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
+import Data from './data'
 
 /**
  * COMPONENT
@@ -19,6 +20,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route path="/map" component={Data} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn && (
