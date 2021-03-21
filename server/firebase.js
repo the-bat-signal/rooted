6674803bbeea01440987556f4350421d02279ca0
Kendall Moraski <kendimoraski@gmail.com>
2021-03-21 14:30:23 -0400

    chore: add firebase.js file to server folder

---
+++ server/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: 'AIzaSyCSikpCX0x6ROlcvaLe8kC1GDTVMvkVVDE',
+  authDomain: 'rooted-4da8a.firebaseapp.com',
+  databaseURL: 'https://rooted-4da8a-default-rtdb.firebaseio.com',
+  projectId: 'rooted-4da8a',
+  storageBucket: 'rooted-4da8a.appspot.com',
+  messagingSenderId: '51008039459'
+})
+
+const db = firebaseApp.firestore()
+
+export {db}
