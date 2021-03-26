554b296a0e375b528b43d0f279c609c595ba7ed5
katelyn devine <katelyndevine.design@gmail.com>
2021-03-26 09:33:23 -0500

    chore: single language compnent vocab db call

--- client/components/SingleLanguage.js
+++ client/components/SingleLanguage.js
@@ -3,14 +3,20 @@ import {db} from '../../server/firebase'
 
 const SingleLanguage = () => {
   const [language, setLanguage] = useState({})
+  const [vocab, setVocab] = useState({})
 
   // remove async await
   useEffect(() => {
     const lang = async () => {
       try {
+        // grabbing language collection
         const data = await db.collection("languages").doc('cherokee').get();
         setLanguage(data.data())
-        console.log('inside useEffect')
+        console.log('inside useEffect first time')
+        //grabbing vocab collection — could a separate call be avoided with relationships?
+        const vocabData = await db.collection("vocab").doc('cherokee_vocab').get();
+        setVocab(vocabData.data())
+        console.log('vocab!-------', vocabData.data())
       } catch (err) {
         console.log('error in SingleLanguage call-----', err)
       }
@@ -24,6 +30,9 @@ const SingleLanguage = () => {
       <h3>{language.name}</h3>
       <h5>Speakers: {language.speakers}</h5>
       <p>{language.description}</p>
+      <p>
+        Are you well?: {vocab['Are you well?']}
+      </p>
     </div>
   )
 }
