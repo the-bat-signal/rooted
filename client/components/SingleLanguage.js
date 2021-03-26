4bbf281c564963fe86d86795a5d1f4ecbee53adb
katelyn devine <katelyndevine.design@gmail.com>
2021-03-26 13:46:31 -0500

    refactor: single lang component to render info from props

--- client/components/SingleLanguage.js
+++ client/components/SingleLanguage.js
@@ -2,35 +2,38 @@ import React, {useState, useEffect} from 'react';
 import {db} from '../../server/firebase'
 
 const SingleLanguage = (props) => {
-  const [language, setLanguage] = useState({})
-  const [vocab, setVocab] = useState({})
+  // const [language, setLanguage] = useState({})
+  // const [vocab, setVocab] = useState({})
+
+  const language = props.location.state.language
 
   // remove async await
-  useEffect(() => {
-    const lang = async () => {
-      try {
-        // grabbing language collection
-        const data = await db.collection("languages").doc('cherokee').get();
-        setLanguage(data.data())
-        console.log('inside useEffect first time')
-        //grabbing vocab collection — could a separate call be avoided with relationships?
-        const vocabData = await db.collection("vocab").doc('cherokee_vocab').get();
-        setVocab(vocabData.data())
-        console.log('vocab!-------', vocabData.data())
-      } catch (err) {
-        console.log('error in SingleLanguage call-----', err)
-      }
-    }
-    lang();
-  }, [])
-
+  // useEffect(() => {
+  //   const lang = async () => {
+  //     try {
+  //       // grabbing language collection
+  //       const data = await db.collection("languages").doc('cherokee').get();
+  //       setLanguage(data.data())
+  //       console.log('inside useEffect first time')
+  //       //grabbing vocab collection — could a separate call be avoided with relationships?
+  //       const vocabData = await db.collection("vocab").doc('cherokee_vocab').get();
+  //       setVocab(vocabData.data())
+  //       console.log('vocab!-------', vocabData.data())
+  //     } catch (err) {
+  //       console.log('error in SingleLanguage call-----', err)
+  //     }
+  //   }
+  //   lang();
+  // }, [])
+  console.log('this is props-----', props)
+  console.log('this is language------', language)
   return (
     <div>
       <h3>{language.name}</h3>
       <h5>Speakers: {language.speakers}</h5>
       <p>{language.description}</p>
       <p>
-        Are you well?: {vocab['Are you well?']}
+        {/* Are you well?: {vocab['Are you well?']} */}
       </p>
     </div>
   )
