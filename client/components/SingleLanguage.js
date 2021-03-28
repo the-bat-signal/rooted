86e9f81c8807d161fdeae8bd1d94fdcec83042c5
Kendall Moraski <kendimoraski@gmail.com>
2021-03-28 18:35:55 -0400

    style: attempt to format single language

--- client/components/SingleLanguage.js
+++ client/components/SingleLanguage.js
@@ -2,38 +2,21 @@ import React, {useState, useEffect} from 'react';
 import {db} from '../../server/firebase'
 
 const SingleLanguage = (props) => {
-  // const [language, setLanguage] = useState({})
-  // const [vocab, setVocab] = useState({})
 
   const language = props.location.state.language
   const vocab = props.location.state.vocab
 
-  // remove async await
-  // useEffect(() => {
-  //   const lang = async () => {
-  //     try {
-  //       // grabbing language collection
-  //       const data = await db.collection("languages").doc('cherokee').get();
-  //       setLanguage(data.data())
-  //       console.log('inside useEffect first time')
-  //       //grabbing vocab collection — could a separate call be avoided with relationships?
-  //       const vocabData = await db.collection("vocab").doc('cherokee_vocab').get();
-  //       setVocab(vocabData.data())
-  //       console.log('vocab!-------', vocabData.data())
-  //     } catch (err) {
-  //       console.log('error in SingleLanguage call-----', err)
-  //     }
-  //   }
-  //   lang();
-  // }, [])
   console.log('this is vocab-----', vocab)
   console.log('this is language------', language)
+
   return (
     <div id="single-language">
       <div id="single-language__header">
         <h1> {language.name} </h1>
         <h4> By learning this language, you are adding to a community of {language.speakers} speakers! </h4>
-        <p>{language.description}</p>
+        <div>
+          <p id="language-description"> {language.description} </p>
+        </div>
       </div>
       <div id="single-language__pronunciation-guide">
 
