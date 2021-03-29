5ad06b03f4e11e4086e8beaa1ecad9733d01c2d0
John M <mccrayjrob@gmail.com>
2021-03-29 11:25:47 -0400

    Fix: StartPage Styling

--- client/components/StartPage.js
+++ client/components/StartPage.js
@@ -2,13 +2,13 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 
-const style = {
+const startButtonStyle = {
   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   borderRadius: 3,
   border: 0,
   color: 'white',
   height: 48,
-  padding: '0 30px',
+  padding: '0 70px',
   boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
 }
 
@@ -19,8 +19,7 @@ export const StartPage = () => {
       <br />
       <div className="whatever">
         <Link to={'/map'}>
-          {/* change back to material ui later */}
-          <Button style={style}>Start</Button>
+          <Button style={startButtonStyle}>Start</Button>
         </Link>
       </div>
     </div>
