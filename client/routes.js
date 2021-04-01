3b6f82e35d031cf8bb4321e41ff75de6929d3fc3
John McCray <mccrayjrob@gmail.com>
2021-04-01 15:19:28 -0400

    Feature: Navbar & Sign In & Sign Up

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Signup, UserHome} from './components'
+import SignIn from './components/Login'
+import SignUp from './components/SignUp'
 import {StartPage} from './components/StartPage'
 import Navbar from './components/navbar'
 import Map from './components/Map'
@@ -26,8 +28,8 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route exact path="/" component={StartPage} />
         <Route path="/map" component={Map} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
+        <Route path="/SignIn" component={SignIn} />
+        <Route path="/SignUp" component={SignUp} />
         <Route path="/:singleLanguage" component={SingleLanguage} />
         {isLoggedIn && (
           <Switch>
@@ -35,8 +37,8 @@ class Routes extends Component {
             <Route path="/home" component={UserHome} />
           </Switch>
         )}
-        {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
+        {/* Displays our SignIn component as a fallback */}
+        {/* <Route component={SignIn} /> */}
       </Switch>
     )
   }
