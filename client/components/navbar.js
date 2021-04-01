ee8e0e88b4f6b89dbc3cd980ddc4db623c64aa65
John McCray <mccrayjrob@gmail.com>
2021-04-01 10:47:50 -0400

    Chore: Minor NavBar Tings

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 export const NavBar = () => {
   return (
     <Navbar collapseOnSelect expand="xl" bg="light" variant="light">
-      <Navbar.Brand href="#/">
+      <Navbar.Brand href="/">
         <img
           alt=""
           width="150"
@@ -19,18 +19,14 @@ export const NavBar = () => {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#features">Placeholder</Nav.Link>
+          <Nav.Link href="#pricing">for later</Nav.Link>
           <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Do</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">we</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">need</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4">a dropdown?</NavDropdown.Item>
           </NavDropdown>
         </Nav>
         <Nav>
