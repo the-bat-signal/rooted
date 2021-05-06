b4bd849c33eaa9f67909beb455b368692ed08211
Kendall Moraski <71110821+kendimoraski@users.noreply.github.com>
2021-05-06 10:52:26 -0400

    Update seed-lang.js

--- public/seed-lang.js
+++ public/seed-lang.js
@@ -4,12 +4,7 @@ const {firebaseAPI} = require ('../secrets')
 
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: firebaseAPI,
-  authDomain: 'rooted-4da8a.firebaseapp.com',
-  databaseURL: 'https://rooted-4da8a-default-rtdb.firebaseio.com',
-  projectId: 'rooted-4da8a',
-  storageBucket: 'rooted-4da8a.appspot.com',
-  messagingSenderId: '51008039459',
+  // keys removed
 })
 
 const db = firebaseApp.firestore()
